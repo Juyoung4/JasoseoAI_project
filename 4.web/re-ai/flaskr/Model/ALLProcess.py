cf248cb63212f46e7ba524bc86ac81d755b5e3ed
Juyoung4 <47167335+Juyoung4@users.noreply.github.com>
2021-08-24 14:44:21 +0900

    [ADD] ALLProcess

---
+++ 4.web/re-ai/flaskr/Model/ALLProcess.py
@@ -0,0 +1,9 @@
+from ReAIPreprocessor import Preprocessor
+from ReAITokenizer import ReAITokenizer
+from ReAIModel import ReAIModel
+# 전처리
+pre = Preprocessor()
+# 토크나이저
+token = ReAITokenizer()
+#모델
+model = ReAIModel()
